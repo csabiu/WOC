42c173a439735ff0d8833a1988bf0476e8796f1e
Cristiano Sabiu <csabiu@gmail.com>
2022-12-07 17:41:23 +0900

    rename

--- woc/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-=====================================================
-woc: 2-D spatial comparison algorithm
-=====================================================
-"""
-
-from woc.woc import *
-from woc.radial_profile import *
